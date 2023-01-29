499588e98b331927c8133c1588f28a4289faf714
innovator-zero <2529300178@qq.com>
2023-01-29 17:16:34 +0800

    2023.01.29 Optimize script

--- pr_curves_base.m
+++ pr_curves_base.m
@@ -3,9 +3,6 @@ addpath(genpath('src/'))
 % Write results in format to use latex code?
 writePR = 0;
 
-% Use precomputed results or evaluate on your computer?
-USEprecomputed = 0;
-
 % Precision-recall measures
 measures = {'fb',...  % Precision-recall for boundaries
     'fop'};   % Precision-recall for Object Proposals
@@ -22,17 +19,15 @@ colors = {'b','g','r','k','m','c','y','b','g','r','k','m','c','y','k','g','b','g
 
 tic;
 % % Evaluate using the correct reading function
-if ~USEprecomputed
-    for ii=1:length(measures)
-        for jj=1:length(methods)
-            % Contours only in 'fb'
-            is_cont = strcmp(methods(jj).type,'contour');
-            if strcmp(measures{ii},'fb') || ~is_cont
-                if exist('cat_id','var')
-                    eval_method_all_params(methods(jj).name, methods(jj).dir, measures{ii}, methods(jj).io_func, database, gt_set, is_cont, cat_id);
-                else
-                    eval_method_all_params(methods(jj).name, methods(jj).dir, measures{ii}, methods(jj).io_func, database, gt_set, is_cont);
-                end
+for ii=1:length(measures)
+    for jj=1:length(methods)
+        % Contours only in 'fb'
+        is_cont = strcmp(methods(jj).type,'contour');
+        if strcmp(measures{ii},'fb') || ~is_cont
+            if exist('cat_id','var')
+                eval_method_all_params(methods(jj).name, methods(jj).dir, measures{ii}, methods(jj).io_func, database, gt_set, is_cont, cat_id);
+            else
+                eval_method_all_params(methods(jj).name, methods(jj).dir, measures{ii}, methods(jj).io_func, database, gt_set, is_cont);
             end
         end
     end
@@ -47,12 +42,8 @@ for kk=1:length(measures)
             if strcmp(methods(ii).type,'contour'),style='--';else, style='-';end
 
             params = get_method_parameters(methods(ii).name);
-
-            if strcmp(database,'SBD')
-                curr_meas = gather_measure(methods(ii).name,params,measures{kk},database,gt_set,cat_id);
-            else
-                curr_meas = gather_measure(methods(ii).name,params,measures{kk},database,gt_set);
-            end
+            curr_meas = gather_measure(methods(ii).name,params,measures{kk},database,gt_set);
+            
             curr_ods  = general_ods(curr_meas);
             curr_ois  = general_ois(curr_meas);
             curr_ap   = general_ap(curr_meas);
